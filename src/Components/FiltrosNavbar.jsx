ca6244cd5e336ba77fcd64cf4dd9f77baf1d7764
Kiofu7w7 <buitragogiraldosantiago@gmail.com>
2024-03-03 16:10:00 -0500

    Arreglos navbar y mejora de grids de productos

--- src/Components/FiltrosNavbar.jsx
+++ src/Components/FiltrosNavbar.jsx
@@ -3,42 +3,42 @@ import React from 'react'
 const FiltrosNavbar = () => {
   return (
     <div className="navbar bg-base-300">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Ofertas</a></li>
-        <li>
-          <a>Categorias</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li><p>Ofertas</p></li>
+            <li>
+              <p>Categorias</p>
+              <ul className="p-2">
+                <li><p>Submenu 1</p></li>
+                <li><p>Submenu 2</p></li>
+              </ul>
+            </li>
+            <li><p>Historial</p></li>
+            <li><p>Vender</p></li>
           </ul>
-        </li>
-        <li><a>Historial</a></li>        
-        <li><a>Vender</a></li>
-      </ul>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><p>Ofertas</p></li>
+          <li>
+            <details>
+              <summary>Categorias</summary>
+              <ul className="p-2 z-10">
+                <li><p>Submenu 1</p></li>
+                <li><p>Submenu 2</p></li>
+              </ul>
+            </details>
+          </li>
+          <li><p>Historial</p></li>
+          <li><p>Vender</p></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+
+      </div>
     </div>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Ofertas</a></li>
-      <li>
-        <details>
-          <summary>Categorias</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Historial</a></li>      
-      <li><a>Vender</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    
-  </div>
-</div>
   )
 }
 
--- src/Containers/Private/LandingPage.jsx
+++ src/Containers/Private/LandingPage.jsx
@@ -3,35 +3,79 @@ import NavbarP from "../../Components/NavbarP";
 import FiltrosNavbar from "../../Components/FiltrosNavbar";
 
 const LandingPage = () => {
+
+  const products = [
+    {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+    {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    }, {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    }, {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    }, {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+  ]
   return (
     <div>
       <NavbarP />
       <FiltrosNavbar />
-      <div class="grid grid-cols-4 gap-4">
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">
-              Shoes!
-              <div className="badge badge-secondary">NEW</div>
-            </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">Fashion</div>
-              <div className="badge badge-outline">Products</div>
+      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 px-10 -z-10">
+        {products.map((product) => (
+          <div key={product.id} className="group relative">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img
+                src={product.imageSrc}
+                alt={product.imageAlt}
+                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+              />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href={product.href}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {product.name}
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">{product.price}</p>
             </div>
           </div>
-        </div>
-        <div>01</div>
-        <div>02</div>
-        <div>03</div>
-        <div>04</div>
-        <div>05</div>
+        ))}
       </div>
     </div>
   );

48ec07894075c80da0403eea6e41219a49027c3a
Kiofu7w7 <buitragogiraldosantiago@gmail.com>
2024-03-10 21:09:38 -0500

    chats styles

--- src/Containers/Private/Chats.jsx
+++ src/Containers/Private/Chats.jsx
@@ -66,50 +66,68 @@ const Chats = () => {
     }
 
     return (
-        <div>
+        <div className="min-h-screen">
             <NavbarP />
-            {
-                data?.map((c,index) => (
-                    <div key={index} onClick={() => { setChatsActual(c); setIndexC(index); console.log(c)}}>
-                        <p>{c.firstName}</p>
-                        <p>{c.lastName}</p>
-                        <img className='size-14' src={c.fotoUrl} alt="" />
+            <div className='min-h-full flex lg:flex-row flex-col justify-between'>
+                <div className='w-full lg:w-3/12 min-h-screen bg-accent px-4 py-4 lg:pb-4"'>
+                    <div className='outline h-full rounded-lg p-2 pt-6 bg-white'>
+                        {
+                            data?.map((c, index) => (
+                                <div key={index} onClick={() => { setChatsActual(c); setIndexC(index); console.log(c) }}>
+                                    <p>{c.firstName}</p>
+                                    <p>{c.lastName}</p>
+                                    <img className='size-14' src={c.fotoUrl} alt="" />
+                                </div>
+                            ))
+                        }
                     </div>
-                ))
-            }
-            <hr />
-            {chatsActual ?
-                <div>
-                    <div>
-                        <p>{chatsActual.firstName}</p>
-                        <p>{chatsActual.lastName}</p>
-                        <img className='size-14' src={chatsActual.fotoUrl} alt="" />
-                    </div>
-                    <hr />
-                    <div>
-                        {chats[indexC].mensajes?.map((m, index) => {
-                            if (m.uid === user.currentUser.uid) {
-                                return (
-                                    <div key={index}>
-                                        <img className='size-14' src={userData.fotoUrl} alt="" />
-                                        <p>{m.mensaje}</p>
-                                    </div>
-                                );
-                            } else {
-                                return (
-                                    <div key={index}>
-                                        <img className='size-14' src={chatsActual.fotoUrl} alt="" />
-                                        <p>{m.mensaje}</p>
-                                    </div>
-                                );
-                            }
-                        })}
-                        <input type='text' value={chatInput}
-                            onChange={handleChatChange}
-                            onKeyPress={handleChatKeyPress}
-                            placeholder='Ab...'></input>
-                    </div>
-                </div> : <div><p>Escoja su chat</p></div>}
+                </div>
+                <div className="w-9/12">
+                    {chatsActual ?
+                        <div>
+                            <div>
+                                <p>{chatsActual.firstName}</p>
+                                <p>{chatsActual.lastName}</p>
+                                <img className='size-14' src={chatsActual.fotoUrl} alt="" />
+                            </div>
+                            <hr />
+                            <div>
+                                {chats[indexC].mensajes?.map((m, index) => {
+                                    if (m.uid === user.currentUser.uid) {
+                                        return (
+                                            <div key={index}>
+                                                <img className='size-14' src={userData.fotoUrl} alt="" />
+                                                <p>{m.mensaje}</p>
+                                            </div>
+                                        );
+                                    } else {
+                                        return (
+                                            <div key={index}>
+                                                <img className='size-14' src={chatsActual.fotoUrl} alt="" />
+                                                <p>{m.mensaje}</p>
+                                                <div className="chat chat-start">
+                                                    <div className="chat-image avatar">
+                                                        <div className="w-10 rounded-full">
+                                                            <img alt="Tailwind CSS chat bubble component" src={chatsActual.fotoUrl} />
+                                                        </div>
+                                                    </div>
+                                                    <div className="chat-bubble">It was said that you would, destroy the Sith, not join them.</div>
+                                                </div>
+                                            </div>
+                                        );
+                                    }
+                                })}
+                                <input type='text' value={chatInput}
+                                    onChange={handleChatChange}
+                                    onKeyPress={handleChatKeyPress}
+                                    placeholder='Ab...'></input>
+                            </div>
+                        </div> : 
+                        <div className='h-full w-full flex justify-center place-items-center'>
+                            <p className='text-3xl'>Aun no has seleccionado un chat</p>
+                        </div>}
+                </div>
+            </div>
             <FooterP />
         </div>
     )

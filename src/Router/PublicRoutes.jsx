aeac978cb46c7e5777406a22b74b807e41583ceb
Kiofu7w7 <buitragogiraldosantiago@gmail.com>
2024-05-21 14:34:56 -0500

    borrador del proyecto

--- src/Router/PublicRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const PublicRouter = ({ isAutentication, children }) => {
-    return !isAutentication ? children : <Navigate to="/*" />;
-};
-
-export default PublicRouter;

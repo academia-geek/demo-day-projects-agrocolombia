b9505a3dfd1f8b2c05aa67888ca691602c771b80
Kiofu7w7 <buitragogiraldosantiago@gmail.com>
2024-05-21 14:34:30 -0500

    borrado del proyecto

--- src/Router/PrivateRoutes.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRouter = ({ isAutentication, children }) => {
-    return isAutentication ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRouter;

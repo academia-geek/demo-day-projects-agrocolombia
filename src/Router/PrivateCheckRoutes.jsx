7f9e111ac23dcb09eddabf2fdac32e24ca3ed734
Kiofu7w7 <buitragogiraldosantiago@gmail.com>
2024-05-21 14:33:34 -0500

    BORRADO DEL PROYECT

--- src/Router/PrivateCheckRoutes.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateCheckRouter = ({ isAutentication, children }) => {
-    return isAutentication === true ? children : <Navigate to="/newcomer" />;
-};
-
-export default PrivateCheckRouter;
